dd8637542ed42fe36aa922fb8717abc80804eac8
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-29 12:02:46 -0500

    done with part two

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,11 @@
 SELECT *
 FROM employees
-WHERE first_name in ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = 'Maya' OR first_name = 'Irena' OR first_name = 'Vidya') AND gender = 'M';
+
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';;
+
 SELECT * FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE  '%qu%';
