8eee0bfdff37d2f62edb97293ec3d47442ea8294
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-29 11:33:42 -0500

    fixed spelling errors

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,18 +1,19 @@
 USE jennifer;
+
 SELECT album_name FROM albums WHERE artist = 'Pink Floyd';
 
 SELECT release_date FROM albums WHERE album_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT genres FROM albums WHERE album_name = 'Nevermind';
+SELECT genre FROM albums WHERE album_name = 'Nevermind';
 
 SELECT album_name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
 SELECT album_name FROM albums WHERE sales < 20;
 
-SELECT album_name FROM albums WHERE genres = 'Rock';
+SELECT album_name FROM albums WHERE genre = 'Rock';
 
 SELECT * FROM albums WHERE
-gennre = 'disco';
+genre = 'disco';
 DELETE FROM albums WHERE genre = 'disco';
 
 SELECT* FROM albums WHERE artist = 'Whitney Houston';
