e8bc05d5d5303c4b38b613e936794588936b6051
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-29 15:29:45 -0500

    done

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -6,4 +6,12 @@ WHERE last_name LIKE 'Z%'
 ORDER BY last_name DESC
 LIMIT 10;
 
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
 
+SELECT emp_no
+FROM salaries
+ORDER BY salary desc
+LIMIT 5 OFFSET 45;
