2cf35ccff6040ca0ba2be82fcc1e48a801b0f7b4
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-30 09:24:03 -0500

    commented out a line with a note attached as to why

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,8 +1,9 @@
 USE jennifer;
-
+# List the first 10 distinct last name sorted in descending order.
 SELECT DISTINCT last_name
 FROM employees
-WHERE last_name LIKE 'Z%'
+# WHERE last_name LIKE 'Z%'
+# DONT DO THAT ^^ BC IF THERE ARE NO LAST NAMES THAT HAVE Z, NOTHING WILL DISPLAY
 ORDER BY last_name DESC
 LIMIT 10;
 
