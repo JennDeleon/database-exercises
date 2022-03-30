7d8f115e0c057a17bc5ee4977dd39868030fcc82
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-03-30 11:26:20 -0500

    dpne

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,41 +1,30 @@
 USE jennifer;
 
-SELECT *
+SELECT CONCAT(first_name, ' ', last_name) as full_name
+# CHANGED THE ABOVE * BC ITS NOT IN GOOD PRACTICE TO ALWAYS USE *
 FROM employees
 WHERE last_name LIKE 'E%'
-ORDER BY last_name;
+ORDER BY emp_no desc;
 
 SELECT *
 FROM employees
 WHERE month(birth_date) = 12
-And day(birth_date) = 25;
-
-SELECT *
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, first_name;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no;
-
-SELECT *
-FROM employees
-WHERE year(hire_date) BETWEEN 1990 AND 1999
-AND month(birth_date) = 12
-AND day(birth_date) = 25;
-
-SELECT *
-FROM employees
-WHERE year(hire_date) BETWEEN 1990 AND 1999
-AND month(birth_date) = 12
 AND day(birth_date) = 25
+AND year(hire_date) BETWEEN 1990 AND 1999
 ORDER BY year(hire_date) DESC
 LIMIT 1;
 
+# CONCAT LOWER FUNCTION WITH ^^^ THAT FUNCTION
+# SELECT *
+# FROM employees
+# WHERE year(hire_date) BETWEEN 1990 AND 1999
+# AND month(birth_date) = 12
+# AND day(birth_date) = 25
+# ORDER BY year(hire_date) DESC
+# LIMIT 1;
+
 SELECT  CONCAT(first_name, ' ', last_name) AS 'Full Name',
-        DATEDIFF(curdate(), hire_date) AS 'Days With Company'
+DATEDIFF(curdate(), hire_date) AS 'days_ working'
 FROM employees
 WHERE year(hire_date) BETWEEN 1990 AND 1999
 AND month(birth_date) = 12
